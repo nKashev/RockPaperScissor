9e4d1f3274b39642dd764c3725270cd6c8804945
Nikolay Kashev <67652045+nKashev@users.noreply.github.com>
2024-05-27 00:32:18 +0300

    Add files via upload

---
+++ Камък, Ножица, Хартия/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Камък__Ножица__Хартия
+{
+    partial class КамъкНожицаХартия
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Време = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(533, 257);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Рестарт";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ИГРАЧ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Победи";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(437, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "КОМПЮТЪР";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(551, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Победи";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(364, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Равни:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(232, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Игри:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(281, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 17);
+            this.label8.TabIndex = 13;
+            // 
+            // Време
+            // 
+            this.Време.Interval = 1000;
+            this.Време.Tick += new System.EventHandler(this.Време_Tick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(361, 6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 31);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Време";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Камък__Ножица__Хартия.Properties.Resources.qq;
+            this.pictureBox2.Location = new System.Drawing.Point(441, 63);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 181);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Камък__Ножица__Хартия.Properties.Resources.qq;
+            this.pictureBox1.Location = new System.Drawing.Point(161, 63);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Камък__Ножица__Хартия.Properties.Resources.paperAnime;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(36, 207);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 90);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Камък__Ножица__Хартия.Properties.Resources.scissorsAnime;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(37, 106);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 90);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Камък__Ножица__Хартия.Properties.Resources.rockAnime;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(37, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 90);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // КамъкНожицаХартия
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 303);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "КамъкНожицаХартия";
+            this.Text = "Камък, ножица, хартия";
+            this.Load += new System.EventHandler(this.КамъкНожицаХартия_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer Време;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Камък, Ножица, Хартия/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Камък__Ножица__Хартия
+{
+    public partial class КамъкНожицаХартия : Form
+    {
+        public int rounds = 50;
+        public int timePerRound = 6;
+        public int playerWins = 0;
+        public int AIWins = 0;
+        public int draws = 0;
+        string[] AIchoice = new string[3] { "ROCK", "PAPER", "SCISSOR" };
+        string command;
+        public int n;
+        Random rnd = new Random();
+        string playerChoice;
+
+
+        public КамъкНожицаХартия()
+        {
+            InitializeComponent();
+            Време.Enabled = true;
+            playerChoice = "none";
+        }
+
+        private void КамъкНожицаХартия_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Време_Tick(object sender, EventArgs e)
+        {
+            timePerRound--;
+            label9.Text = Convert.ToString(timePerRound);
+            label8.Text = Convert.ToString(rounds);
+
+            if (timePerRound < 1)
+            {
+                Време.Enabled = false;
+
+                timePerRound = 6;
+                n = rnd.Next(0, 3);
+                command = AIchoice[n];
+
+                switch (command)
+                {
+                    case "ROCK":
+                        pictureBox2.Image = Properties.Resources.rock;
+                        break;
+
+                    case "PAPER":
+                        pictureBox2.Image = Properties.Resources.paper;
+                        break;
+
+                    case "SCISSOR":
+                        pictureBox2.Image = Properties.Resources.scissors;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (rounds > 1)
+                {
+                    checkGame();
+                }
+                else if (rounds == 1)
+                {
+                    checkGame();
+                    decisionEngine();
+                }
+            }
+        }
+        private void checkGame()
+        {
+            if (playerChoice == "ROCK" && command == "SCISSOR")
+            {
+                MessageBox.Show("Ти победи");
+                playerWins += 1;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "ROCK" && command == "PAPER")
+            {
+                MessageBox.Show("Компютъра победи");
+                AIWins += 1;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "PAPER" && command == "ROCK")
+            {
+                MessageBox.Show("Ти победи");
+                playerWins += 1;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "PAPER" && command == "SCISSOR")
+            {
+                MessageBox.Show("Компютъра победи");
+                AIWins += 1;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "SCISSOR" && command == "ROCK")
+            {
+                MessageBox.Show("Компютъра победи");
+                AIWins += 1;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "SCISSOR" && command == "PAPER")
+            {
+                MessageBox.Show("Ти победи");
+                playerWins += 1;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show("ИЗБЕРИ!");
+                nextRound();
+            }
+            else
+            {
+                MessageBox.Show("Равни");
+                draws += 1;
+                rounds--;
+                nextRound();
+            }
+        }
+
+        private void decisionEngine()
+        {
+            if (playerWins > AIWins)
+            {
+                MessageBox.Show(label1.Text + " спечели играта");
+            }
+            else if (playerWins < AIWins)
+            {
+                MessageBox.Show("Компютърът спечели играта");
+            }
+            else if (playerWins == AIWins)
+            {
+                MessageBox.Show("Равенство");
+            }
+        }
+
+        private void nextRound()
+        {
+            playerChoice = "none";
+            pictureBox1.Image = Properties.Resources.qq;
+            Време.Enabled = true;
+            pictureBox2.Image = Properties.Resources.qq;
+            label8.Text = Convert.ToString(rounds);
+            label2.Text = Convert.ToString(playerWins);
+            label4.Text = Convert.ToString(AIWins);
+            label6.Text = Convert.ToString(draws);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playerChoice = "ROCK";
+            pictureBox1.Image = Properties.Resources.rock;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playerChoice = "PAPER";
+            pictureBox1.Image = Properties.Resources.paper;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playerChoice = "SCISSOR";
+            pictureBox1.Image = Properties.Resources.scissors;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timePerRound = 6;
+            nextRound();
+        }
+    }
+}
